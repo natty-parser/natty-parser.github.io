dc4a9a22b1ce10729c599cb06c9a8e9bf74c5931
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-11-13 00:10:08 +0100

    released 1.1.1

--- javascript/try.js
+++ javascript/try.js
@@ -16,7 +16,7 @@ window.setup = async function setup() {
 		})
 
 		const prefix = "/app/";
-    const classpath = `${prefix}natty-1.1.1-SNAPSHOT.jar:${prefix}antlr-runtime-3.5.3.jar:${prefix}slf4j-api-2.0.17.jar:${prefix}slf4j-nop-2.0.17.jar`;
+    const classpath = `${prefix}natty-1.1.1.jar:${prefix}antlr-runtime-3.5.3.jar:${prefix}slf4j-api-2.0.17.jar:${prefix}slf4j-nop-2.0.17.jar`;
 		console.log("Classpath:", classpath);
 		const cj = await cheerpjRunLibrary(classpath);
 
