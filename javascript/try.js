c112f7e1c686be1ac1887b224b06560df0adfa3d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-01-18 13:43:58 +0100

    Fixed groupid

--- javascript/try.js
+++ javascript/try.js
@@ -7,35 +7,35 @@ let setupPromise = null;
 
 window.setup = async function setup() {
 	if (setupPromise === null) {
-    setupPromise = (async () => {
-	if (window.nattyParser === undefined) {
-		function showPreloadProgress(preloadDone, preloadTotal) {
-			const percentage = Math.round((preloadDone * 100) / preloadTotal);
-			console.log(percentage + "%");
-		}
-		await cheerpjInit({
-			enableDebug: false,
-			version: 17,
-			javaProperties: properties,
-			// cjGetRuntimeResources();
-			preloadResources: {"/lt/17/lib/tzdb.dat":[0,131072],"/lt/17/lib/modules":[0,131072,1179648,3801088,3932160,4063232,4194304,5242880,5636096,5898240,6029312,6291456,6815744,7077888,7864320,7995392,9568256,9699328,9830400,10223616,19005440,19136512,19660800,19791872,37486592,37617664,38010880,38141952],"/lt/17/conf/logging.properties":[0,131072],"/lt/17/conf/security/java.security":[0,131072],"/lt/17/jre/lib/cheerpj-jsobject.jar":[0,131072],"/lt/17/jre/lib/cheerpj-awt.jar":[0,131072],"/lt/17/jre/lib/cheerpj-handlers.jar":[0,131072],"/lt/etc/users":[0,131072],"/lt/etc/localtime":[]},
-			preloadProgress: showPreloadProgress
-		})
+		setupPromise = (async () => {
+			if (window.nattyParser === undefined) {
+				function showPreloadProgress(preloadDone, preloadTotal) {
+					const percentage = Math.round((preloadDone * 100) / preloadTotal);
+					console.log(percentage + "%");
+				}
+				await cheerpjInit({
+					enableDebug: false,
+					version: 17,
+					javaProperties: properties,
+					// cjGetRuntimeResources();
+					preloadResources: {"/lt/17/lib/tzdb.dat":[0,131072],"/lt/17/lib/modules":[0,131072,1179648,3801088,3932160,4063232,4194304,5242880,5636096,5898240,6029312,6291456,6815744,7077888,7864320,7995392,9568256,9699328,9830400,10223616,19005440,19136512,19660800,19791872,37486592,37617664,38010880,38141952],"/lt/17/conf/logging.properties":[0,131072],"/lt/17/conf/security/java.security":[0,131072],"/lt/17/jre/lib/cheerpj-jsobject.jar":[0,131072],"/lt/17/jre/lib/cheerpj-awt.jar":[0,131072],"/lt/17/jre/lib/cheerpj-handlers.jar":[0,131072],"/lt/etc/users":[0,131072],"/lt/etc/localtime":[]},
+					preloadProgress: showPreloadProgress
+				})
 
-		const prefix = "/app/lib/";
-    const classpath = `${prefix}natty-1.1.2.jar:${prefix}antlr-runtime-3.5.3.jar`;
-		console.log("Classpath:", classpath);
-		const cj = await cheerpjRunLibrary(classpath);
+				const prefix = "/app/lib/";
+				const classpath = `${prefix}natty-1.1.2.jar:${prefix}antlr-runtime-3.5.3.jar`;
+				console.log("Classpath:", classpath);
+				const cj = await cheerpjRunLibrary(classpath);
 
 
-		const Parser = await cj.org.natty.Parser;
-		window.nattyParser = await new Parser();
+				const Parser = await cj.org.natty.Parser;
+				window.nattyParser = await new Parser();
 
-		console.log("Parser loaded", window.nattyParser);
+				console.log("Parser loaded", window.nattyParser);
+			}
+			return window.nattyParser;
+		})();
 	}
-	return window.nattyParser;
-	 })();
-  }
-  return setupPromise;
+	return setupPromise;
 }
 window.setup();
